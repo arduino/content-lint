63e61530a8b796ef19773427540b19cbee0bbb69
Sebastian Romero <s.romero@arduino.cc>
2022-08-26 16:30:40 +0200

    Add unit tests for naming

--- test/naming.test.js
+++ test/naming.test.js
@@ -1,20 +1,37 @@
 import { Article } from '../domain/article.js'
-import { validateFolderName } from '../validations/naming.js';
+import { validateFolderName, validateAssetsFolderName } from '../validations/naming.js';
 
 const articleA = new Article();
 articleA.path = "./demo/this_is_Not_compliant";
 
 const articleB = new Article();
 articleB.path = "./demo/this-should-pass";
+articleB.rawData = "![](assets/dummy.png)"
+
+const articleC = new Article();
+articleC.rawData = "![](img/dummy.png)";
 
 test('Tests if non-compliant article folder name is detected', () => {
     const errors = validateFolderName(articleA);
-    console.log(errors);
+    // console.log(errors);
     expect(errors.length).toBe(2);
 });
 
 test('Tests if compliant article folder name passes validation', () => {
   const errors = validateFolderName(articleB);
-  console.log(errors);
+  // console.log(errors);
+  expect(errors.length).toBe(0);
+});
+
+
+test('Tests if incorrect assets name fails validation', () => {
+  const errors = validateAssetsFolderName(articleC, "assets");
+  // console.log(errors);
+  expect(errors.length).toBe(1);
+});
+
+test('Tests if correct assets name passes validation', () => {
+  const errors = validateAssetsFolderName(articleB, "assets");
+  // console.log(errors);
   expect(errors.length).toBe(0);
 });
