fb50b4d800cc62bdeb8f9d306a1fcec7b151eac3
Sebastian Romero <s.romero@arduino.cc>
2024-05-17 09:40:32 +0200

    Add option to ignore folder names

--- validations/naming.js
+++ validations/naming.js
@@ -2,26 +2,38 @@ import { ValidationIssue } from '../domain/validation-issue.js';
 
 /**
  * Checks if the foldername contains any discouraged underscores, spaces or upper-case letters.
- * @param {Article} article 
+ * @param {Article} article The article to validate
+ * @param {Array} ignoreList A list of folder names to ignore
  * @returns an array containg the occurred validation errors
  */
-function validateFolderName(article) {
+function validateFolderName(article, ignoreList = []) {
     let errorsOccurred = [];
     const issueType = ValidationIssue.Type.ERROR;
 
-    if(article.path.indexOf("_") != -1){
-        const errorMessage = "Folder path uses discouraged underscore.";
-        errorsOccurred.push(new ValidationIssue(errorMessage, article.path, issueType));
-    }
+    const folders = article.path.split("/").filter((folder) => {
+        // Filter out empty strings and relative paths
+        return folder != "" && folder != "." && folder != "..";
+    });
 
-    if(article.path.indexOf(" ") != -1){
-        const errorMessage = "Folder path uses discouraged blanks.";
-        errorsOccurred.push(new ValidationIssue(errorMessage, article.path, issueType));
-    }
+    for (let folder of folders) {
+        if(ignoreList.includes(folder)){
+            continue;
+        }
 
-    if(article.path != article.path.toLowerCase()){
-        const errorMessage = "Folder path uses discouraged non-lower case characters.";
-        errorsOccurred.push(new ValidationIssue(errorMessage, article.path, issueType));
+        if(folder.indexOf("_") != -1){
+            const errorMessage = "Folder path uses discouraged underscore.";
+            errorsOccurred.push(new ValidationIssue(errorMessage, article.path, issueType));
+        }
+    
+        if(folder.indexOf(" ") != -1){
+            const errorMessage = "Folder path uses discouraged blanks.";
+            errorsOccurred.push(new ValidationIssue(errorMessage, article.path, issueType));
+        }
+    
+        if(folder != folder.toLowerCase()){
+            const errorMessage = "Folder path uses discouraged non-lower case characters.";
+            errorsOccurred.push(new ValidationIssue(errorMessage, article.path, issueType));
+        }
     }
 
     return errorsOccurred;
